ffec7780c970c87335c6a475c8c9fbbabc7f0eb2
Wiermag <wierzbowska.magda@gmail.com>
2020-07-12 22:44:38 +0200

    fix firebase

--- repeat-app/src/firebase.js
+++ repeat-app/src/firebase.js
@@ -1,14 +1,15 @@
 import * as firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC1VwexjGyMePF5JQ4TGQejrn-gbvG14pI",
-  authDomain: "magda-app-34c7b.firebaseapp.com",
-  databaseURL: "https://magda-app-34c7b.firebaseio.com",
-  projectId: "magda-app-34c7b",
-  storageBucket: "magda-app-34c7b.appspot.com",
-  messagingSenderId: "182805958148",
-  appId: "1:182805958148:web:b06af80205eae8b8fdef51"
-};
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN, 
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+}
+
 firebase.initializeApp(firebaseConfig);
 
 const firebaseFold = require('firebase/app');
